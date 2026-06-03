7eb0d93172cba102db014a7d8488079919e26976
chenjian2664 <chenjian2664@gmail.com>
2026-06-03 08:24:14 +0800

    Fix the PatResolver package declaration after the move

--- backend/src/main/java/com/bytequay/app/service/credentials/PatResolver.java
+++ backend/src/main/java/com/bytequay/app/service/credentials/PatResolver.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.bytequay.app.web;
+package com.bytequay.app.service.credentials;
 
 import com.bytequay.app.domain.CredentialType;
 import com.bytequay.app.service.CredentialService;
